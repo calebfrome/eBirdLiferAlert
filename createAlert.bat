13cd256b4f593900a92cdd081911eb00fa2bb068
calebfrome <calebfrome@ou.edu>
2019-02-11 12:52:40 -0600

    fixed batch file, added details to about.txt

--- createAlert.bat
+++ createAlert.bat
@@ -1,11 +1,5 @@
 :: Wrapper batch file to run createAlert.py ::
 
 ::@echo OFF
-
-:: Location of createAlert.py (if not set, current directory used)
-set scriptPath=C:/Users/Caleb/Documents/Python/eBirdLiferAlert
-
-:: Run python script ::
-cd %scriptPath%
-python createAlert.py
+venv\Scripts\python.exe createAlert.py
 ::cmd \k
