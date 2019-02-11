bd7c341150768d18bcb0d39889837898ddc8be73
calebfrome <calebfrome@ou.edu>
2019-02-10 20:48:46 -0600

    migrated entire project from AHK to web scraping

--- createAlert.py
+++ createAlert.py
@@ -1,123 +1,118 @@
-# -*- coding: utf-8 -*-
-
-import xlrd
 import subprocess
-import time
+from bs4 import BeautifulSoup
+import mechanize
+import http.cookiejar as cj
 
-get_new_data = True
-numAlertSources = 7
+alert_url_prefix = 'https://ebird.org/ebird/alert/summary?sid='
+alert_sids = {'TX': 'SN10387', 'OK': 'SN10380', 'NM': 'SN10376', 'AZ': 'SN10347', 'CO': 'SN10349', 'KS': 'SN10360',
+              'AR': 'SN10346', 'LA': 'SN10362'}
+
+life_list_url = 'https://ebird.org/MyEBird?cmd=lifeList&listType=world&listCategory=default&time=life'
+aba_list_url = 'https://ebird.org/MyEBird?cmd=lifeList&listType=aba&listCategory=default&time=life'
+
+login_url = 'https://secure.birds.cornell.edu/cassso/login?service=https%3A%2F%2Febird.org%2Flogin%2Fcas%3Fportal%3Debird&locale=en_US'
 
 
 class Observation:
-
-    def __init__(self, species, count, date, location, county, state):
+    def __init__(self, species, count, date, checklist_link, location, map_link, county, state):
         self.species = species
         self.count = count
         self.date = date
+        self.checklist_link = checklist_link
         self.location = location
+        self.map_link = map_link
         self.county = county
         self.state = state
 
     def output(self):
-        return [self.species, self.date, self.location, self.county, self.state]
+        return [self.species, self.date, self.location, '<a href=' + self.map_link + '>Map</a>', self.county,
+                self.state, '<a href=' + self.checklist_link + '>Checklist</a>']
 
 
 def main():
-    if get_new_data:
-        # clear raw.txt
-        open("raw.txt", 'w').close()
-
-        # run the AHK script to grab the raw data from ebird
-        subprocess.Popen("getAlertData.exe getAlertData.ahk", shell=True)
-        time.sleep(15 * numAlertSources)
-
-    # open raw.txt
-    file = open("raw.txt")
-
-    database = []
-    line = ""
-    index = 0
-    species = ""
-    count = ""
-    date = ""
-    location = ""
-    county = ""
-    state = ""
-
-    # loop through each alert section
-    for x in range(numAlertSources):
-
-        # truncate lines until start of data
-        while True:
-            line = file.readline()
-            if line[:11] == "Needs Alert":
-                state = line[16:]
-            if line[:7] == "Species":
-                break
-
-        # read remainder of file into Observation objects, stored in database
-        # read until we see (c) Cornell...
-        line = file.readline()
-        while line[1] != " ":
-            index = line.find("(")
-            species = line[:(index - 1 if index >= 0 else -1)]
-            file.readline()  # don't keep latin name
-            line = file.readline()
-            # if there is a confirmed/unconfirmed tag, don't keep it
-            if line == "CONFIRMED\n" or line == "UNCONFIRMED\n":
-                line = file.readline()
-            # determine whether count is X (empty) or a number
-            if line[0].isdigit():
-                count = line[0]
-                date = line[2:(line.find(","))]
-            else:
-                count = "X"
-                date = line[:(line.find(","))]
-            file.readline()  # don't keep checklist link
-            location = file.readline()[:-1]
-            file.readline()  # don't keep map link
-            line = file.readline()
-            county = line[:(line.find("\t"))]
-            line = file.readline()
-            # if there is a show details link, don't keep it
-            if line == "SHOW DETAILS\n":
-                line = file.readline()
-            # create Observation object and add to database
-            database.append(Observation(species, count, date, location, county, state))
-
-    # compare results with AWM database to determine which would be lifers
-    workbook = xlrd.open_workbook('C:/Users/Caleb/Documents/Birds/AMW.xlsx')
-    sheet = workbook.sheet_by_name("ABA 7.7.1")
-    row = 0
+    # Read ABA checklist
     aba_list = []
+    aba_list_file = open('aba_list.txt')
+    for species in aba_list_file.readlines():
+        aba_list.append(species.strip())
+
+    # Read exceptions
+    exceptions_list = []
+    exceptions_file = open('exceptions.txt')
+    for species in exceptions_file.readlines():
+        exceptions_list.append(species.strip())
+
+    # Read credentials
+    credentials = {}
+    credentials_file = open('credentials.txt')
+    credentials['username'] = credentials_file.readline().strip()
+    credentials['password'] = credentials_file.readline().strip()
+
+    br = mechanize.Browser()
+    bcj = cj.LWPCookieJar()
+    br.set_cookiejar(bcj)
+
+    # Browser options
+    br.set_handle_equiv(True)
+    br.set_handle_gzip(True)
+    br.set_handle_redirect(True)
+    br.set_handle_referer(True)
+    br.set_handle_robots(False)
+    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+    br.addheaders = [('User-agent', 'Chrome')]
+
+    # Open the eBird login page
+    br.open(login_url)
+    # Select the login form
+    br.select_form(nr=0)
+    # Set credentials
+    br.form['username'] = credentials['username']
+    br.form['password'] = credentials['password']
+    # Submit the login form
+    br.submit()
+
+    # Scrape life list
+    print('scraping life list')
     life_list = []
+    life_list_html = BeautifulSoup(br.open(life_list_url).read(), 'html.parser')
+    for a in life_list_html.find_all(attrs={'data-species-code': True}):
+        life_list.append(str.strip(a.text))
+
+    # Scrape eBird alerts
+    print('scraping eBird alerts')
+    observation_list = []
+    for alert_sid in alert_sids.values():
+        alert_html = BeautifulSoup(br.open(alert_url_prefix + alert_sid).read(), 'html.parser')
+        for tr in alert_html.find_all('tr', class_='has-details'):
+            species_name = str.strip(tr.findChild(class_='species-name').findChild('a').text)
+            species_count = str.strip(tr.findChild(class_='count').text)
+            species_date = str.strip(tr.findChild(class_='date').text)[:-10]  # truncate 'Checklist'
+            species_checklist_link = 'https://ebird.org' + str.strip(tr.findChild(class_='date').findChild('a')['href'])
+            species_location = str.strip(tr.findChild(class_='location').text)
+            species_map_link = str.strip(tr.findChild(class_='location').findChild('a')['href'])
+            species_county = str.strip(tr.findChild(class_='county').text)
+            species_state = str.strip(tr.findChild(class_='state').text).split(',')[0]  # truncate ', United States'
+            observation_list.append(Observation(species_name, species_count, species_date, species_checklist_link,
+                                                species_location, species_map_link, species_county, species_state))
+
+    # determine which species would be lifers
+    print('creating custom alert')
+    observation_list.sort(key=lambda x: x.state, reverse=True)
+    observation_list.sort(key=lambda x: x.species, reverse=False)
     lifer_needs = []
-
-    while True:
-        if str(sheet.cell(row, 0).value) == xlrd.empty_cell.value:
-            break
-        aba_list.append(sheet.cell(row, 0).value)
-        if sheet.cell(row, 1).value == "X":
-            life_list.append(sheet.cell(row, 0).value)
-        row += 1
-
-    database.sort(key=lambda x: x.species, reverse=False)
-    database.sort(key=lambda x: x.state, reverse=True)
-    life_list.sort()
-    aba_list.sort()
-
-    for o in database:
-        if o.species in aba_list and not o.species in life_list:
+    for o in observation_list:
+        if o.species in aba_list and o.species not in life_list and o.species not in exceptions_list:
             lifer_needs.append(o)
 
-    # build html file for output
+    # build output html file
     output = open("output.html", "w")
     output.write("<html><body><h1>eBird Lifer Alert</h1>")
     if len(lifer_needs) == 0:
         output.write("<p>No lifers reported.</p>")
     else:
         output.write(
-            "<table border=1 style=border-collapse:collapse cellpadding=3><tr><th>Species</th><th>Date</th><th>Location</th><th>County</th><th>State</th></tr>")
+            '<table border=1 style=border-collapse:collapse cellpadding=3><tr><th>Species</th><th>Date</th>'
+            '<th>Location</th><th>Map Link</th><th>County</th><th>State</th><th>Checklist Link</th></tr>')
         for l in lifer_needs:
             output.write("<tr>")
             for x in range(len(l.output())):
